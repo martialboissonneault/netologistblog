d141c133c0429a6a395c8a4c2d8d2d32369a9113
Martial Boissonneault <mboissonneault@interbec.com>
2023-04-12 08:56:49 -0400

    Add svelte-adapter-azure-swa

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
+import azure from 'svelte-adapter-azure-swa';
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: azure()
 	}
 };
-
 export default config;
